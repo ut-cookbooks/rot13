c806794ca043f3b3f309b9dbb5254c1886fdfff1
Fletcher Nichol <fnichol@nichol.ca>
2014-02-27 11:18:39 -0700

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,10 @@
-name "rot13"
-version "1.0.0"
+name              "rot13"
+version           "1.0.0"
+
+maintainer        "Fletcher Nichol"
+maintainer_email  "fnichol@nichol.ca"
+license           "Apache 2.0"
+description       "A super secure rot13 Chef cookbook, you know, cause it had to be done."
+
+supports "ubuntu"
+supports "centos"
